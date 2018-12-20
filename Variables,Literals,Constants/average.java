0c6ffb83842274ca56d2cfae136f3e1ccca15763
Vedaant-rajoo <vedaant12345@gmail.com>
2018-12-20 14:39:07 +0530

    second

---
+++ Variables,Literals,Constants/average.java
@@ -0,0 +1,13 @@
+public class average{
+    public static void main(String[] args){
+        int first=11;
+        int second=20;
+        double average;
+        average=(first+second) / 2;
+        double tie;
+        tie=average + 2.0;
+        System.out.println("The new No.: " + tie);
+        System.out.println("Average: "+average);
+
+    }
+}
