ac6dab646ba95ae764cdc8959d3150b934ae574a
Vedaant-rajoo <vedaant12345@gmail.com>
2018-12-20 13:12:10 +0530

    first

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("hello World!!");
+        System.out.println("print ln works fine with this method!");
+        System.out.println("Heloo Again!!");
+    }
+}
