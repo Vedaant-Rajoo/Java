6293568f07c4a094658ff6d3d8b134b0c1b096a4
Vedaant-rajoo <vedaant12345@gmail.com>
2018-12-20 16:25:10 +0530

    go

---
+++ Variables,Literals,Constants/Char.java
@@ -0,0 +1,8 @@
+public class Char{
+    public static void main(String[] args) {
+        char letter='j';
+        char eter2='g';
+        
+        System.out.println(letter+eter2);
+    } 
+}
---
+++ Variables,Literals,Constants/string.java
@@ -0,0 +1,14 @@
+public class string{
+    public static void main(String[] args){
+        String name="John";
+        String surname="Rambo";
+        System.out.println(name+" "+surname);
+        boolean value;
+        value=false;
+        System.out.println(value);
+        char letter;
+        letter='j';
+        
+        System.out.println(letter);
+    }
+}
