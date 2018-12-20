eb1d9072fbf7a898b451ce5980bbc4395264dc87
Vedaant-rajoo <vedaant12345@gmail.com>
2018-12-20 20:03:52 +0530

    DescionStructures: create

---
+++ DescionStructures.java
@@ -0,0 +1,25 @@
+package io.github.jiangdequan;
+
+import javax.swing.JOptionPane;
+
+public class DescionStructures {
+    public static void main(String[] args) {
+        int number;
+        String input;
+        input=JOptionPane.showInputDialog("Please enter a number: ");
+        number=Intger.parseInt(input);
+        
+        if (number==5){
+            JOptionPane.showMessageDialog(null, "the number is exactly 5");     
+
+        }
+        if (number>5){
+            JOptionPane.showMessageDialog(null, "The number is greater than 5");
+        }
+        if (number>10){
+            JOptionPane.showMessageDialog(null, "The number is greater than 10");
+        }
+
+        System.exit(0);
+    }     
+}
